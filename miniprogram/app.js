9c12609ae46564783adb7b12bb110b4313942e79
米高哥 <qc_69799f1c597a48e2b692280097438c7a@git.code.tencent.com>
2020-02-13 22:27:05 +0800

    feat: 初始化小程序端界面及文件

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,20 +1,104 @@
 //app.js
 App({
   onLaunch: function () {
-    
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
       wx.cloud.init({
-        // env 参数说明：
-        //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
-        //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
-        //   如不填则使用默认环境（第一个创建的环境）
-        // env: 'my-env-id',
         traceUser: true,
       })
     }
-
-    this.globalData = {}
+    wx.getSystemInfo({
+      success: e => {
+        this.globalData.StatusBar = e.statusBarHeight;
+        let capsule = wx.getMenuButtonBoundingClientRect();
+        if (capsule) {
+          this.globalData.Custom = capsule;
+          this.globalData.CustomBar = capsule.bottom + capsule.top - e.statusBarHeight;
+        } else {
+          this.globalData.CustomBar = e.statusBarHeight + 50;
+        }
+      }
+    })
+  },
+  globalData: {
+    ColorList: [{
+      title: '嫣红',
+      name: 'red',
+      color: '#e54d42'
+    },
+    {
+      title: '桔橙',
+      name: 'orange',
+      color: '#f37b1d'
+    },
+    {
+      title: '明黄',
+      name: 'yellow',
+      color: '#fbbd08'
+    },
+    {
+      title: '橄榄',
+      name: 'olive',
+      color: '#8dc63f'
+    },
+    {
+      title: '森绿',
+      name: 'green',
+      color: '#39b54a'
+    },
+    {
+      title: '天青',
+      name: 'cyan',
+      color: '#1cbbb4'
+    },
+    {
+      title: '海蓝',
+      name: 'blue',
+      color: '#0081ff'
+    },
+    {
+      title: '姹紫',
+      name: 'purple',
+      color: '#6739b6'
+    },
+    {
+      title: '木槿',
+      name: 'mauve',
+      color: '#9c26b0'
+    },
+    {
+      title: '桃粉',
+      name: 'pink',
+      color: '#e03997'
+    },
+    {
+      title: '棕褐',
+      name: 'brown',
+      color: '#a5673f'
+    },
+    {
+      title: '玄灰',
+      name: 'grey',
+      color: '#8799a3'
+    },
+    {
+      title: '草灰',
+      name: 'gray',
+      color: '#aaaaaa'
+    },
+    {
+      title: '墨黑',
+      name: 'black',
+      color: '#333333'
+    },
+    {
+      title: '雅白',
+      name: 'white',
+      color: '#ffffff'
+    },
+    ],
+    memberid: '',
+    openid: ''
   }
 })

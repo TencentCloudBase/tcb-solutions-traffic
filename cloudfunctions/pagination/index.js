01abcf0ff193e830b18304aa312261023059a364
walkingp <walkingp@126.com>
2020-02-16 12:36:28 +0800

    feat: 首页查询功能，添加公共: (#9)

---
+++ cloudfunctions/pagination/index.js
@@ -0,0 +1,26 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+const db = cloud.database()
+
+// 公共分页函数
+exports.main = async (event, context) => {
+  const dbName = event.dbName;
+  let { filter, pageIndex, pageSize } = event;
+  filter = filter ? filter : null;
+  pageIndex = pageIndex ? pageIndex : 1;
+  pageSize = pageSize ? pageSize : 10;
+  const countResult = await db.collection(dbName).where(filter).count();
+  const total = countResult.total;
+  const totalPage = Math.ceil(total / pageSize);
+  let hasMore = true;
+  if(pageIndex >= totalPage){
+    hasMore = false;
+  }
+
+  return db.collection(dbName).where(filter).skip((pageIndex - 1) * pageSize).limit(pageSize).get().then(res=>{
+    res.hasMore = hasMore;
+    return res;
+  })
+}
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -20,22 +20,23 @@ Page({
       id: 2,
       type: 'image',
         url: 'cloud://demo-3ee737.3f9c-demo-3ee737-1257366989/3.png'
-    }]
+    }],
+    resultList: []
   },
 
-  // TODO:
   searchVehicle(){
     wx.cloud.callFunction({
-      name: 'searchVehicle',
+      name: 'pagination',
       data: {
-        vehicle_type: this.data.picker[this.data.index],
-        vehicle_date: this.data.vehicle_date,
-        limit: 10,
-        skip: 0
-        // vehicle_date
+        dbName: 'vehicle',
+        pageIndex: 1,
+        pageSize: 10
       }
     }).then(res=>{
       console.log(res);
+      this.setData({
+        resultList: res.result.data
+      })
     })
 
   },

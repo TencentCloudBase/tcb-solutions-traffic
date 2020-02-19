110574a604ee8cdbe89f0de21c5466c582394b9b
humengting <1130558141@qq.com>
2020-02-19 11:31:36 +0800

    feat:历史轨迹对接 (#21)
    * 消息通知查看
    
    * 历史轨迹增加
    
    * 1.历史轨迹列表展示

--- miniprogram/pages/track/track.js
+++ miniprogram/pages/track/track.js
@@ -5,16 +5,32 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    pageCount: 1
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getInfoList();
   },
 
+  getInfoList() {
+    const pageCount = this.data.pageCount;
+    let trackList = pageCount == 1 ? [] : this.data.trackList;
+    wx.cloud.callFunction({
+      name: 'historicalRouteList',
+      data: {
+        pageIndex: this.data.pageCount
+      }
+    }).then(res => {
+      console.log(res);
+      res.result.data!=""&& this.setData({
+        trackList: trackList.concat(res.result.data),
+        pageCount: pageCount + 1
+      })
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -46,15 +62,13 @@ Page({
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function () {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    this.getInfoList();
   },
 
   /**
